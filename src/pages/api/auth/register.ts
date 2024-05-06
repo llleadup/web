1695744272c9d50aa36454202a52c433eb80b2c6
dankorotenko <danilkorotenko2@gmail.com>
2024-05-06 20:26:50 +0200

    added resend audience adding

--- src/pages/api/auth/register.ts
+++ src/pages/api/auth/register.ts
@@ -1,5 +1,8 @@
 import type { APIRoute } from "astro";
 import { supabase } from "../../../lib/supabase";
+import { Resend } from 'resend';
+
+const resend = new Resend('re_DzGSrAuN_LbYjwzjGGW5nkfVEpGUeEG8d');
 
 export const POST: APIRoute = async ({ request, redirect }) => {
   const formData = await request.formData();
@@ -9,6 +12,14 @@ export const POST: APIRoute = async ({ request, redirect }) => {
   if (!email || !password) {
     return new Response("Email and password are required", { status: 400 });
   }
+
+  resend.contacts.create({
+    email: email,
+    firstName: 'First',
+    lastName: 'Last',
+    unsubscribed: false,
+    audienceId: 'fb92bb2a-3130-4c15-848d-04102351d601',
+  });
 
   const { error } = await supabase.auth.signUp({
     email,
